61450501f06471040cb00dac97ca9a948cedd3a8
emanherawy <eman.herawy@gmaail.com>
2024-07-13 08:51:00 +0200

    init contract using foundry and add uniswap and ccip examples

--- uniswap/smart-contracts/src/GasPriceFeesHook.sol
+++ uniswap/smart-contracts/src/GasPriceFeesHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
